b2ebcd4abe096ae0fd4518badd585e6d8914a7a7
warjiang <1096409085@qq.com>
2026-06-08 00:59:19 +0800

    Add terminal detection utilities and update project configuration

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,6 +1,6 @@
-use terminal_detection::add;
+// use terminal_detection::add;
 fn main() {
-    let result = add(1, 2);
-    println!("Result of add(1, 2) is: {}", result);
+    // let result = add(1, 2);
+    // println!("Result of add(1, 2) is: {}", result);
     println!("Hello, world!");
 }
